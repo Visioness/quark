6b9e029ca8bf757109671a787706c6e540ee6f11
Visioness <aygun-08-zurnaci@hotmail.com>
2026-02-24 01:16:56 +0300

    refactor: force socket transport as websocket only

--- client/src/socket.js
+++ client/src/socket.js
@@ -4,5 +4,6 @@ const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000';
 
 export const socket = io(API_BASE_URL, {
+  transports: ['websocket'],
   autoConnect: false,
 });
